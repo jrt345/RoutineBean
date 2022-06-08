b45556ea8f2bebb590f59a6ae0fae9e8af7c2728
Jrt345 <JonTech543@protonmail.com>
2022-06-08 00:16:16 -0700

    Add button onAction functions to Controller.java

--- src/main/java/com/example/routinebean/controllers/Controller.java
+++ src/main/java/com/example/routinebean/controllers/Controller.java
@@ -1,5 +1,22 @@
 package com.example.routinebean.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class Controller {
 
+    @FXML
+    void newRoutine(ActionEvent event) {
+
+    }
+
+    @FXML
+    void openRoutine(ActionEvent event) {
+
+    }
+
+    @FXML
+    void openSettings(ActionEvent event) {
+
+    }
 }
