d3ca90d99630031e68988f6b480f01e35b826892
Jrt345 <JonTech543@protonmail.com>
2022-06-30 11:23:32 -0700

    Implement methods and variables in RoutineProperties.java

--- src/main/java/com/example/routinebean/utils/properties/RoutineProperties.java
+++ src/main/java/com/example/routinebean/utils/properties/RoutineProperties.java
@@ -1,5 +1,67 @@
 package com.example.routinebean.utils.properties;
 
+import com.example.routinebean.utils.AppData;
+import javafx.stage.Stage;
+
+import java.io.*;
+import java.util.Properties;
+
 public class RoutineProperties {
 
+    private static Stage stage;
+    private static double width;
+    private static double height;
+
+    public static Stage getStage() {
+        return stage;
+    }
+
+    public static void setStage(Stage stage) {
+        RoutineProperties.stage = stage;
+    }
+
+    public static double getWidth() {
+        return width;
+    }
+
+    public static void setWidth(double width) {
+        RoutineProperties.width = width;
+    }
+
+    public static double getHeight() {
+        return height;
+    }
+
+    public static void setHeight(double height) {
+        RoutineProperties.height = height;
+    }
+
+    public static void loadDefaultProperties(String folder) throws IOException {
+        width = 916;
+        height = 639;
+        write(folder);
+        load(folder);
+    }
+
+    public static void write(String folder) throws IOException {
+        Properties properties = new Properties();
+        OutputStream os = new FileOutputStream(AppData.ROUTINE_DIRECTORY.concat(folder + "\\Routine.properties"));
+
+        properties.setProperty("routine-window-width", String.valueOf(width));
+        properties.setProperty("routine-window-height", String.valueOf(height));
+
+        properties.store(os, null);
+    }
+
+    public static void load(String folder) throws IOException, NullPointerException, NumberFormatException {
+        Properties properties = new Properties();
+        InputStream inputStream = new FileInputStream(AppData.ROUTINE_DIRECTORY.concat(folder + "\\Routine.properties"));
+        properties.load(inputStream);
+
+        width = Double.parseDouble(properties.getProperty("routine-window-width"));
+        height = Double.parseDouble(properties.getProperty("routine-window-height"));
+
+        stage.setWidth(width);
+        stage.setHeight(height);
+    }
 }
