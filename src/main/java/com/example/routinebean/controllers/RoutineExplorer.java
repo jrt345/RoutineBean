38d5a5537201b456b8985f13d10015dcd9447a5d
Jrt345 <JonTech543@protonmail.com>
2022-07-01 06:49:19 -0700

    Implement ui outline for routineExplorer.fxml

--- src/main/java/com/example/routinebean/controllers/RoutineExplorer.java
+++ src/main/java/com/example/routinebean/controllers/RoutineExplorer.java
@@ -1,5 +1,17 @@
 package com.example.routinebean.controllers;
 
-public class RoutineExplorer {
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
 
+public class RoutineExplorer {
+    @FXML
+    private VBox routineVBox;
+
+    @FXML
+    private Button updateButton;
+
+    @FXML
+    private ScrollPane vBoxScrollPane;
 }
