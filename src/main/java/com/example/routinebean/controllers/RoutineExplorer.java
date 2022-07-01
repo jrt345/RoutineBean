4e83f3cc9251a4f9090e86c3cba12a5f5f7bbdac
Jrt345 <JonTech543@protonmail.com>
2022-07-01 08:16:21 -0700

    Made RoutineExplorer.java initializable

--- src/main/java/com/example/routinebean/controllers/RoutineExplorer.java
+++ src/main/java/com/example/routinebean/controllers/RoutineExplorer.java
@@ -4,6 +4,7 @@ import com.example.routinebean.App;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -13,9 +14,11 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
 import java.util.Objects;
+import java.util.ResourceBundle;
 
-public class RoutineExplorer {
+public class RoutineExplorer implements Initializable {
     @FXML
     private VBox routineVBox;
 
@@ -32,5 +35,11 @@ public class RoutineExplorer {
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+    }
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
     }
 }
