96b03e919c00ba572eee836bed4674360c301968
Jrt345 <JonTech543@protonmail.com>
2022-07-01 08:15:25 -0700

    Add returnToMainMenu function in routineExplorer.fxml and RoutineExplorer.java

--- src/main/java/com/example/routinebean/controllers/RoutineExplorer.java
+++ src/main/java/com/example/routinebean/controllers/RoutineExplorer.java
@@ -1,9 +1,19 @@
 package com.example.routinebean.controllers;
 
+import com.example.routinebean.App;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
 
 public class RoutineExplorer {
     @FXML
@@ -14,4 +24,13 @@ public class RoutineExplorer {
 
     @FXML
     private ScrollPane vBoxScrollPane;
+
+    @FXML
+    private void returnToMainMenu(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(App.class.getResource("routineBean.fxml")));
+        Stage stage = (Stage)((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 }
