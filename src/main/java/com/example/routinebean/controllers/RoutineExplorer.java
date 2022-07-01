c194b3a1258946adc480ce9c46fdeae485948b3c
Jrt345 <JonTech543@protonmail.com>
2022-07-01 15:00:20 -0700

    Made buttons in routineVBox open their routine and also add context menu for each button

--- src/main/java/com/example/routinebean/controllers/RoutineExplorer.java
+++ src/main/java/com/example/routinebean/controllers/RoutineExplorer.java
@@ -12,6 +12,8 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -24,6 +26,10 @@ import java.util.Objects;
 import java.util.ResourceBundle;
 
 public class RoutineExplorer implements Initializable {
+
+    private File[] files;
+    private ArrayList<Routine> routines;
+
     @FXML
     private VBox routineVBox;
 
@@ -47,12 +53,7 @@ public class RoutineExplorer implements Initializable {
         AppUtils.newRoutine();
     }
 
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-        File file = new File(AppData.ROUTINE_DIRECTORY);
-
-        File[] files = file.listFiles(File::isDirectory);
-
+    private ArrayList<Routine> getRoutines(File[] files) {
         ArrayList<Routine> routines = new ArrayList<>();
         for (int i = 0; i < Objects.requireNonNull(files).length; i++) {
             try {
@@ -62,23 +63,75 @@ public class RoutineExplorer implements Initializable {
             }
         }
 
+        return routines;
+    }
+
+    private void deleteRoutine(String directory, int index) {
+        boolean isRoutineDeleted = AppUtils.deleteRoutine(directory);
+
+        if (isRoutineDeleted) {
+            routines = getRoutines(files);
+            files = new File(AppData.ROUTINE_DIRECTORY).listFiles(File::isDirectory);
+            routineVBox.getChildren().remove(index);
+        }
+    }
+
+    private ContextMenu generateContextMenu(Button button, String directory, int index) {
+        ContextMenu contextMenu = new ContextMenu();
+
+        MenuItem open = new MenuItem("Open");
+        MenuItem explorer = new MenuItem("Open in Explorer");
+        MenuItem delete = new MenuItem("Delete");
+
+        open.setOnAction(event -> button.fire());
+        explorer.setOnAction(event -> {
+            try {
+                AppUtils.openExplorer(directory);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        delete.setOnAction(event -> deleteRoutine(directory, index));
+
+        contextMenu.getItems().add(open);
+        contextMenu.getItems().add(explorer);
+        contextMenu.getItems().add(delete);
+
+        return contextMenu;
+    }
+
+    private Button generateButton(String title, String directory, int index) {
+        Button button = new Button(title);
+        button.setPrefSize(Double.MAX_VALUE, 40);
+        button.setMinHeight(40);
+        button.setOnAction(event -> {
+            try {
+                AppUtils.openRoutine(directory);
+            } catch (IOException | ClassNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        button.setContextMenu(generateContextMenu(button, directory, index));
+
+        return button;
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        File file = new File(AppData.ROUTINE_DIRECTORY);
+
+        files = file.listFiles(File::isDirectory);
+        routines = getRoutines(files);
+
         for (int i = 0; i < files.length; i++) {
             if (routines.get(i) != null){
-                Button button = new Button(routines.get(i).getTitle());
-                button.setMinHeight(40);
-                button.setPrefSize(Double.MAX_VALUE, 40);
-                int index = i;
-                button.setOnAction(event -> {
-                    try {
-                        AppUtils.openRoutine(files[index].getName());
-                    } catch (IOException | ClassNotFoundException e) {
-                        throw new RuntimeException(e);
-                    }
-                });
-
-                routineVBox.getChildren().add(button);
+                String directory = files[i].getName();
+                String title = routines.get(i).getTitle();
+                routineVBox.getChildren().add(generateButton(title, directory, i));
                 routineVBox.setFillWidth(true);
             }
+
         }
 
         updateButton.setOpacity(0.0);
