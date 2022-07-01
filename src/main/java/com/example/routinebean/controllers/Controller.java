3dd06e62796b1a88cf5bfbb2561777055e4db8c1
Jrt345 <JonTech543@protonmail.com>
2022-07-01 08:13:47 -0700

    Change openRoutine function in Controller.java to open routineExplorer.fxml

--- src/main/java/com/example/routinebean/controllers/Controller.java
+++ src/main/java/com/example/routinebean/controllers/Controller.java
@@ -4,11 +4,17 @@ import com.example.routinebean.App;
 import com.example.routinebean.utils.AppUtils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.Objects;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
@@ -22,8 +28,12 @@ public class Controller implements Initializable {
     }
 
     @FXML
-    private void openRoutine(ActionEvent event) throws IOException, ClassNotFoundException {
-        AppUtils.openRoutine(App.getStage());
+    private void openRoutine(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(App.class.getResource("routineExplorer.fxml")));
+        Stage stage = (Stage)((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 
     @FXML
