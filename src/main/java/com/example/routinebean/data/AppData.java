a5ce16439b6f2c789dc8ceddb90d5b908c4a1869
Jrt345 <JonTech543@protonmail.com>
2022-07-16 14:00:54 -0700

    Replace AppData.java serialize and deserialize methods with try-with-resources statements to prevent memory leaks

--- src/main/java/com/example/routinebean/data/AppData.java
+++ src/main/java/com/example/routinebean/data/AppData.java
@@ -11,20 +11,14 @@ public class AppData {
     }
 
     public static void serialize(String directory, Routine routine) throws IOException {
-        FileOutputStream fileOut = new FileOutputStream(serializedRoutine(directory));
-        ObjectOutputStream out = new ObjectOutputStream(fileOut);
-        out.writeObject(routine);
-        out.close();
-        fileOut.close();
+        try (FileOutputStream fileOut = new FileOutputStream(serializedRoutine(directory)); ObjectOutputStream out = new ObjectOutputStream(fileOut)) {
+            out.writeObject(routine);
+        }
     }
 
     public static Routine deserialize(String directory) throws IOException, ClassNotFoundException {
-        FileInputStream fileIn = new FileInputStream(serializedRoutine(directory));
-        ObjectInputStream in = new ObjectInputStream(fileIn);
-        Routine routine = (Routine) in.readObject();
-        in.close();
-        fileIn.close();
-
-        return routine;
+        try (FileInputStream fileIn = new FileInputStream(serializedRoutine(directory)); ObjectInputStream in = new ObjectInputStream(fileIn)) {
+            return (Routine) in.readObject();
+        }
     }
 }
