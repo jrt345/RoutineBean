99d34d3af151e27b3abd2843026d14640b4924df
Jrt345 <JonTech543@protonmail.com>
2022-08-05 11:45:26 -0700

    Add org.apache.commons.io dependency to pom.xml and module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,12 +2,13 @@ module com.example.routinebean {
     requires javafx.controls;
     requires javafx.fxml;
     requires com.google.gson;
+    requires org.apache.commons.io;
 
     opens com.example.routinebean to javafx.fxml;
     exports com.example.routinebean;
     exports com.example.routinebean.utils;
-    exports com.example.routinebean.controllers;
     opens com.example.routinebean.controllers to javafx.fxml;
+    exports com.example.routinebean.controllers;
     exports com.example.routinebean.properties;
     opens com.example.routinebean.data to com.google.gson;
     exports com.example.routinebean.data;
